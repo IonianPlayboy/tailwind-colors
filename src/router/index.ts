bf4f1f79890636cabbc4aacebb8f6f2fe8b5c862
Nathanael Lafeuille <akai.chesha@gmail.com>
2021-03-10 15:58:36 +0100

    Add netlify.toml and Error page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 const Color = () => import("@/views/Color.vue");
+const ErrorPage = () => import("@/views/Error.vue");
 
 const routes = [
 	{ path: "/", component: Home },
 	{ path: "/color/:key", component: Color },
+	{ path: "/:catchAll(.*)", component: ErrorPage },
 ];
 
 const router = createRouter({
---
+++ src/views/Error.vue
@@ -0,0 +1,18 @@
+<template>
+	<main class="min-h-screen flex flex-col">
+		<section class="m-auto w-4/6">
+			<h1 class="text-5xl font-bold">Oops</h1>
+			<p class="text-2xl mt-8">
+				This page does not seem to exist. Sorry about that.
+			</p>
+			<p
+				class="mt-6 text-xl underline hover:text-teal-400 transition-colors duration-150 ease-out"
+			>
+				<router-link to="/">Go back to the home page</router-link>
+			</p>
+		</section>
+	</main>
+</template>
+
+<script lang="ts"></script>
+<script setup lang="ts"></script>
