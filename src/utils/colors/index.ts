e61699e095a3be845cc9c792043bb34825b5a839
Nathanael Lafeuille <akai.chesha@gmail.com>
2021-03-18 12:41:48 +0100

    Improve finding closest shades logic and handle edge cases for custom shades

--- src/utils/colors/index.ts
+++ src/utils/colors/index.ts
@@ -1,9 +1,11 @@
 import { Ref } from "vue";
 
-type RGB = "red" | "green" | "blue";
+export type RGB = "red" | "green" | "blue";
 type RGBValues = [redValue: number, greenValue: number, blueValue: number];
 
-const getValuesFromColor = (currColor: string) => ({
+export const getValuesFromHexCode = (
+	currColor: string
+): Record<RGB, number> => ({
 	red: parseInt(currColor.substring(1, 3), 16),
 	green: parseInt(currColor.substring(3, 5), 16),
 	blue: parseInt(currColor.substring(5, 7), 16),
@@ -14,6 +16,21 @@ const formatHexValue = (currValue: number) =>
 
 const formatHexColor = (red: number, green: number, blue: number) =>
 	`#${formatHexValue(red)}${formatHexValue(green)}${formatHexValue(blue)}`;
+
+export const calculateRGBGap = (
+	baseHexCode: string,
+	otherHexCode: string
+): Record<RGB, number> => {
+	const baseRGBValues = getValuesFromHexCode(baseHexCode);
+	const otherRBGValues = getValuesFromHexCode(otherHexCode);
+	return (Object.entries(baseRGBValues) as Array<[RGB, number]>).reduce(
+		(result, [key, value]) => ({
+			...result,
+			[key]: otherRBGValues[key] - value,
+		}),
+		{} as Record<RGB, number>
+	);
+};
 
 const calculateNewRGBValue = (
 	currValue: number,
@@ -26,26 +43,22 @@ const calculateNewRGBValue = (
 	);
 
 const adjustColor = (
-	closestColor: string,
-	nearestColor: string,
+	closestHexCode: string,
+	nearestHexCode: string,
 	amount: number
 ) => {
-	// console.log(`closestColor : ${closestColor}`);
-	// console.log(`nearestColor : ${nearestColor}`);
-	// console.log(`amount : ${amount}`);
-	const closestValues = getValuesFromColor(closestColor);
-	const nearestValues = getValuesFromColor(nearestColor);
-
-	// console.log(`closestValues : `, closestValues);
-	// console.log(`nearestValues : `, nearestValues);
+	const closestValues = getValuesFromHexCode(closestHexCode);
+	const nearestValues = getValuesFromHexCode(nearestHexCode);
 	const adjustedValues = (Object.entries(closestValues) as Array<
 		[RGB, number]
 	>).map(([currKey, currValue]) => {
 		const currGap = nearestValues[currKey] - currValue;
-		console.log(`${currKey} currGap : ${currGap}`);
-		return calculateNewRGBValue(currValue, currGap, Math.abs(amount));
+		return calculateNewRGBValue(
+			currValue,
+			nearestHexCode === "#000000" ? currGap / 8 : currGap,
+			Math.abs(amount)
+		);
 	}) as RGBValues;
-	// console.log(`adjustedValues : ${adjustedValues}`);
 	return formatHexColor(...adjustedValues);
 };
 
@@ -55,31 +68,28 @@ export const getCustomColor = (
 ): string => {
 	const foundShade = shadesList[shadeNumber];
 	if (foundShade) return foundShade;
-	const closestShade = findClosestShade(shadeNumber, shadesList);
-	const nearestShade = findClosestShade(
-		shadeNumber,
-		shadesList,
-		closestShade
-	);
+	const [closestShade, nearestShade] = findClosestShades(shadeNumber);
+	const nearestHexCode =
+		shadesList[nearestShade as number] ??
+		(shadeNumber > closestShade ? "#000000" : "#ffffff");
 	return adjustColor(
 		shadesList[closestShade],
-		shadesList[nearestShade],
+		nearestHexCode,
 		closestShade - shadeNumber
 	);
 };
 
-const findClosestShade = (
-	currValue: number,
-	currShades: Record<string | number, string>,
-	shadeToExclude?: number
-) =>
-	Object.keys(currShades).reduce((result, key) => {
-		if (!result) return Number(key);
-		if (shadeToExclude && shadeToExclude === Number(key)) return result;
-		return Math.abs(Number(key) - currValue) < Math.abs(result - currValue)
-			? Number(key)
-			: result;
-	}, 0);
+const findClosestShades = (
+	currValue: number
+): [ClosestShade: number, NearestShade: number | null] => {
+	if (currValue > 900) return [900, null];
+	if (currValue < 50) return [50, null];
+	if (currValue < 100) return currValue <= 75 ? [50, 100] : [100, 50];
+	const closestShade = Math.round(currValue / 100) * 100;
+	const nearestShade =
+		closestShade + (currValue >= closestShade ? 1 : -1) * 100;
+	return [closestShade, nearestShade];
+};
 
 export const getCurrHexValue = (shadeNumber: number): string => {
 	const hexValue = Math.round((shadeNumber / 900) * 255);
